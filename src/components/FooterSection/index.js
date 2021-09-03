c24e34a3c4aec1a1fb61c84f3f56fa424006c29d
Christina <cphanth@gmail.com>
2021-09-03 14:25:40 -0700

    add agents photos to file system

---
+++ src/components/FooterSection/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { IconContext } from 'react-icons/lib';
+import {FaFacebook, FaYoutube, FaInstagram, FaLinkedin } from 'react-icons/fa';
+
+
+const Footer = () => {
+    return(
+        <div>
+            <IconContext.Provider>
+                
+            </IconContext.Provider>
+        </div>
+    )
+}
